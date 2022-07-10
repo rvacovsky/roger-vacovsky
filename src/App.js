2c9e270e5fc9ea878e85c2b8f18b69bd7cf2ae9c
Roger Vacovsky <rvacovsky@gmail.com>
2022-07-10 16:17:35 -0400

    initial component setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import ContactForm from './components/Contact';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <Nav
+      categories={categories}
+      setCurrentCategory={setCurrentCategory}
+      currentCategory={currentCategory}
+      contactSelected={contactSelected}
+      setContactSelected={setContactSelected}
+    ></Nav>
+    <main>
+      {!contactSelected ? (
+        <>
+          <About></About>
+          <Portfolio currentCategory={currentCategory}></Portfolio>
+        </>
+      ) : (
+        <ContactForm></ContactForm>
+      )}
+    </main>
+  </div>
+);
   );
 }
 
